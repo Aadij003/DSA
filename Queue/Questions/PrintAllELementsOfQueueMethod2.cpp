9c938284cf41d49083ba1ed440bda53058590223
Aadi Jain <aadij9388@gmail.com>
2026-09-19 13:23:14 +0530

    Create PrintAllELementsOfQueueMethod2.cpp

---
+++ Queue/Questions/PrintAllELementsOfQueueMethod2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<int> q;
+  q.push(10); 
+   q.push(20); 
+    q.push(30); 
+     q.push(40); 
+      q.push(50); 
+      int n=q.size();
+while(n--){
+cout<<q.front()<<endl;
+q.push(q.front());
+q.pop();
+}
+   
+}
